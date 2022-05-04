ba90036d0a1d2930516c1aab422516266d6d0b28
Analira Scalabrini <analira.scalabrini@gmail.com>
2022-05-04 09:46:48 -0400

    feat: svgr config and showing logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import { Box, Tab, Tabs, Typography } from '@mui/material';
 import React from 'react';
+import Logo from '../assets/logo-s.svg';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -37,14 +38,29 @@ const Home: NextPage = () => {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Typography fontWeight="400" fontSize={48} color="primary" align="center">
-        Soul Divina
-      </Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          margin: 2
+        }}
+      >
+        <Logo style={{ height: 80, width: 80, marginRight: 16 }} />
+        <Typography
+          fontWeight="bold"
+          fontSize={48}
+          color="primary"
+          align="center"
+        >
+          Soul Divina
+        </Typography>
+      </Box>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs
           value={value}
           onChange={handleChange}
-          aria-label="basic tabs example"
           textColor="secondary"
           indicatorColor="secondary"
           centered
