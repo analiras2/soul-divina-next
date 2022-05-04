07b8e0ecc5344d907d4d57ada4e9683b56879235
Analira Scalabrini <analira.scalabrini@gmail.com>
2022-05-03 23:10:19 -0400

    feat: material ui setup for web

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,67 @@
 import type { NextPage } from 'next';
+import { Box, Tab, Tabs, Typography, Grid } from '@mui/material';
+import React from 'react';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
 
 const Home: NextPage = () => {
-  return <h1>Soul Divina</h1>;
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          textColor="secondary"
+          indicatorColor="secondary"
+          centered
+        >
+          <Tab label="Ervas" />
+          <Tab label="Banhos" />
+          <Tab label="Quem somos" />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        Ervas
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Banhos
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Soul Divina
+      </TabPanel>
+    </Box>
+  );
 };
 
 export default Home;
